1f5f741aaee2eae1e6ffd5b770fb5e8cdf23fe1b
deveshdatwani <datwanidevesh@gmail.com>
2026-01-18 10:03:07 -0500

    wip

--- edter/edter.py
+++ edter/edter.py
@@ -1,3 +1,12 @@
 from vit.vit import transformer
+from torch import nn
 
-tf = transformer()
+
+class edter(nn.Module):
+    def __init__(self, depth=24, in_dim=128):
+        self.depth = depth
+        self.transformer_block = transformer(in_dim=in_dim)
+        self.positional_embedding = nn.Parameter()
+    
+    def forward(self,):
+        return None
